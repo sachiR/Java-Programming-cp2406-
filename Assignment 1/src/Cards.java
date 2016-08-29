32b9d529cf771214de3c0af4a90db43db09860c1
sachiR <sachini.perera@my.jcu.edu.au>
2016-08-29 13:17:42 +1000

    Plagioclase Card

--- Assignment 1/src/Cards.java
+++ Assignment 1/src/Cards.java
@@ -5,7 +5,6 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 
 
-
 public class Cards {
     //Specifying keys of the mineral cards
     private String title, chemistry,classification,crystalSystem,occurrence,cleavage,crustalAbundance,economicValue;
@@ -27,26 +26,32 @@ public class Cards {
         this.cardImage = image;
     }
 
+    //Hardness is returned
     public double Hardness(){
        return hardness;
     }// ends Hardness()
 
+    //Specific Gravity if returned
     public double Gravity(){
         return specificGravity;
     }// ends Gravity()
 
+    //Cleavage is returned
     public String Cleavage(){
         return cleavage;
     }//ends Cleavage()
 
+    //Crustal Abundance is returned
     public String Abundance(){
         return crustalAbundance;
     }//ends Abundance()
 
+    //Economic Value is returned
     public String Value(){
         return economicValue;
     }//ends Value()
 
+    //Image of the card is returned
     public BufferedImage GetCardImage(){
         return cardImage;
     }//ends GetCardImage()
@@ -54,21 +59,26 @@ public class Cards {
     public static void main(String args[]) throws IOException {
         // need to change occurrence to array
         Cards Quartz = new Cards("Quartz","SiO_2","tectosilicate","hexagonal","igneous,metamorphic,sedimentary","poor/none","high","moderate",7,2.65, ImageIO.read(new File("images\\Slide01.jpg")));
-
+        Cards Plagioclase = new Cards("Plagioclase","Na Al Si_3 O_8 - Ca Al_2 Si_2 O_8","tectosilicate","triclinic","igneous,metamorphic,sedimentary","1 perfect, 1 good","very high","moderate",6-6.5,2.6-2.8,ImageIO.read(new File("images\\Slide02.jpg")));
         //create a JFrame to display the card
-        JFrame window = new JFrame("trump of mineral");
-        window.setSize(400,600);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
+        JFrame window = new JFrame("Mineral Supertrumps");
+        window.setSize(900,600);
 
         //create a JPanel
         JPanel contentPane = new JPanel(new BorderLayout());
 
         //create a JLabel
         JLabel cardLabel = new JLabel(new ImageIcon(Quartz.GetCardImage()));
-        cardLabel.setSize(300,400);
+        cardLabel.setSize(100,150);
+
+        JLabel cardLabel2 = new JLabel(new ImageIcon(Plagioclase.GetCardImage()));
+        cardLabel2.setSize(100,150);
 
         contentPane.add(cardLabel);
+        contentPane.add(cardLabel2);
         window.add(contentPane);
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     } //main() ends here
 } //class ends here
---
+++ Assignment 1/src/Deck.java
@@ -0,0 +1,18 @@
+import javax.smartcardio.Card;
+
+/**public class Deck {
+    private Card[] deck;
+    private int currentCard; // index of the next card to be dealt
+
+    //Constructor to build a deck of cards
+    public Deck {
+        String[] title = {"Quartz","Plagioclase"};
+        String[] chemistry = {"SiO_2","Na Al Si_3 O_8 - Ca Al_2 Si_2 O_8"};
+        String[] images =
+        deck = new Card[66];
+        currentCard = 0;
+
+
+    }
+}
+ **/
