cf4d4ca56c03d8bf8ffa5debd8bcad55385b87e0
sachiR <sachini.perera@my.jcu.edu.au>
2016-08-30 17:56:37 +1000

    Entry Message and continue message

--- Assignment 1/src/App.java
+++ Assignment 1/src/App.java
@@ -7,9 +7,25 @@ public class App {
         JOptionPane option = new JOptionPane();
         JFrame frame = new JFrame();
         String name;
-        String continueProcess = new String();
+        String answer ;
+        //gets the name form the user
         name = option.showInputDialog("Welcome To Mineral Supertrumps\n           Please Enter Your Name");
-        continueProcess = option.showInputDialog(name +", Would you like to continue...\n    Y - Yes,\n    N - No");
-    }
-}
+
+        //if the name is invalid it will ask again
+        while(name.isEmpty()){
+            name = option.showInputDialog("Try Again...\nPlease Enter Your Name");
+        } //while ends
+
+        // option to continue or not
+        if (JOptionPane.showConfirmDialog(null,name+ ", Would you like to continue...", "Continue",
+                option.YES_NO_OPTION) == option.YES_OPTION) {
+            // yes option
+        } else {
+            option.showMessageDialog(null, "Thank You.."+ name+" See You Later");
+            System.exit(0);
+        }//else ends
+
+
+    }//main() ends
+}// class ends
 
